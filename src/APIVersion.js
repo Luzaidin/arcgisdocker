f94d632ddf35d2254ccf41988f10f6a2c49af0c1
Luis Belo <luzaidinbelo@gmail.com>
2021-01-23 21:07:52 -0300

    Feat: add project example to Talks

---
+++ src/APIVersion.js
@@ -0,0 +1,3 @@
+const options = { version: '4.18', css: true }
+
+export default options
---
+++ src/views/SceneView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="viewDiv"></div>
+</template>
+
+<script>
+import { loadModules } from 'esri-loader'
+import options from '../APIVersion'
+
+export default {
+  name: 'SceneView',
+  methods: {
+    createScene () {
+      loadModules([
+        'esri/Map',
+        'esri/views/SceneView'
+      ], {
+        options
+      }).then(([Map, SceneView]) => {
+        const map = new Map({
+          basemap: 'satellite',
+          ground: 'world-elevation'
+        })
+        const sceneView = new SceneView({
+          container: 'viewDiv',
+          map
+        })
+
+        console.log('sceneView', sceneView)
+      })
+    }
+  },
+  mounted () {
+    this.createScene()
+  }
+
+}
+</script>
+
+<style>
+
+#viewDiv {
+  height: 800px;
+  width: 100%;
+}
+</style>
